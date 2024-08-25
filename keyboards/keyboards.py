689c56c6ea31cc5131740449ad514bbea2aa1579
Muhammad Akhmadov <magaakhmadov@mail.ru>
2024-08-25 17:28:45 +0300

    Update services, other_handlers, user_handlers

--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -16,7 +16,7 @@ yes_no_kb_builder.row(button_yes, button_no, width=2)
 # Создаем клавиатуру с кнопками "Давай!" и "Не хочу!"
 yes_no_kb: ReplyKeyboardMarkup = yes_no_kb_builder.as_markup(
     one_time_keyboard=True,
-    rezise_keyboard=True
+    resize_keyboard=True
 )
 
 # Создаем кнопки игровой клавиатуры
